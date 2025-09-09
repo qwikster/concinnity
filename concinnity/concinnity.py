f9b76a236f28234f92ae482c6dc4957a431b24e0
qwikster <ocoincs@gmail.com>
2025-09-08 20:37:33 -0400

    directory and project setup

---
+++ concinnity/concinnity.py
@@ -0,0 +1,15 @@
+import math
+import time
+import os
+import sys
+import configparser
+from pathlib import Path
+
+import schedule
+
+def main():
+    print("silly")
+    pass
+
+if __name__ == "__main__":
+    main()
